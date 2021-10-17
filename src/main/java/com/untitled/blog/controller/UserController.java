ff7cebb0c506b8144ad3f5364ddd843f8e2db423
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2021-10-17 12:18:25 +0530

    syntax issue fixed

--- src/main/java/com/untitled/blog/controller/UserController.java
+++ src/main/java/com/untitled/blog/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(("/user"))
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
